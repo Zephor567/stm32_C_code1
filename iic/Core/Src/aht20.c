615aa9254c97f790e626432e3ea75efddc55bdf2
Zephor567 <1713053212@qq.com>
2026-07-17 16:37:46 +0800

    刚刚上传失败了再试一次

---
+++ iic/Core/Src/aht20.c
@@ -0,0 +1,53 @@
+#include "aht20.h"
+#include "stm32f1xx_hal_def.h"
+#define AHT20_ADDRESS 0x70//设置外设为70，读取外设为71，hal函数会自动设置无需操心
+
+uint8_t readBuffer[6] = {0};
+
+void AHT20_Init(){
+	uint8_t readBuffer;
+	HAL_Delay(40);//根据数据手册，该外设延时40ms后工作
+	HAL_I2C_Master_Receive(&hi2c1, AHT20_ADDRESS, &readBuffer, 1, HAL_MAX_DELAY);
+	if ((readBuffer & 0x08) == 0x00){
+		uint8_t sendBuffer[3] = { 0xBE, 0x08, 0x00};
+		HAL_I2C_Master_Transmit(&hi2c1, AHT20_ADDRESS, sendBuffer, 3, HAL_MAX_DELAY);
+
+	}
+}
+
+
+void AHT20_Read(float *Temperature, float *Humidity){
+	uint8_t sendBuffer[3] = { 0xAC, 0x33, 0x00};
+	uint8_t readBuffer[6] = {0};
+
+	HAL_I2C_Master_Transmit_DMA(&hi2c1, AHT20_ADDRESS, sendBuffer, 3);//非阻塞函数瞬间完成后续交给中断机制
+	HAL_Delay(75);
+	HAL_I2C_Master_Receive_DMA(&hi2c1, AHT20_ADDRESS, readBuffer, 6);//数组本身就是地址所以readBuffer这不用取地址符
+
+	if ((readBuffer[0] & 0x80) == 0x00){
+		uint32_t data = 0;
+		data = ((uint32_t)readBuffer[3] >> 4) + ((uint32_t)readBuffer[2] << 4) + ((uint32_t)readBuffer[1] << 12);
+		*Humidity = data * 100.0f / (1 << 20);//2的20次方即向左移20位
+
+		data = (((uint32_t)readBuffer[3] & 0x0F) << 16) + ((uint32_t)readBuffer[4] << 8) + (uint32_t)readBuffer[5];
+		*Temperature = data * 200.0f /(1 << 20) - 50;
+	}
+}
+
+void AHT_Measure(){
+	static uint8_t sendBuffer[3] = { 0xAC, 0x33, 0x00};//static防止该函数结束后数据被释放回收。
+	HAL_I2C_Master_Transmit_IT(&hi2c1, AHT20_ADDRESS, sendBuffer, 3);
+}
+
+void AHT20_GET(){
+	HAL_I2C_Master_Receive_IT(&hi2c1, AHT20_ADDRESS, readBuffer, 6);//数组本身就是地址所以readBuffer这不用取地址符
+}
+
+void AHT20_Analysis(float *Temperature, float *Humidity){
+	HAL_I2C_Master_Receive(&hi2c1, AHT20_ADDRESS, readBuffer, 1, HAL_MAX_DELAY);
+		if ((readBuffer[0] & 0X08) == 0X00){
+			uint8_t sendBuffer[3] = { 0xBE, 0x08, 0x00};
+			HAL_I2C_Master_Transmit(&hi2c1, AHT20_ADDRESS, sendBuffer, 3, HAL_MAX_DELAY);
+
+		}
+}
